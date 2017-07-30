a80beaab518cfc0cf03735cef955c15b56bdd253
Lucas Duarte Anício <lucasduarte599@gmail.com>
2017-07-30 20:32:00 -0300

    removes unused code

--- PointOfSale.Mvc/Controllers/HomeController.cs
+++ PointOfSale.Mvc/Controllers/HomeController.cs
@@ -1,25 +1,11 @@
-﻿using PointOfSale.Application;
-using PointOfSale.Application.Apps;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PointOfSale.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly CategoryApplication _product;
-
-        public HomeController(CategoryApplication product)
-        {
-            _product = product;
-        }
-
         public ActionResult Index()
         {
-            var a = _product.List();
             return View();
         }
 
--- PointOfSale.Mvc/Controllers/PaymentMethodsController.cs
+++ PointOfSale.Mvc/Controllers/PaymentMethodsController.cs
@@ -2,9 +2,6 @@
 using PointOfSale.Application.ViewModels;
 using PointOfSale.Mvc.Notifications;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace PointOfSale.Mvc.Controllers
